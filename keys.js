70f2c0574ebfdd24c6275e4c59afd969cf8c10d7
itsHel <51739304+itsHel@users.noreply.github.com>
2022-04-05 18:43:37 +0200

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,13 @@
+const token = "";
+const yandexKey = "key=";
+const imdbKey = "";
+const tenorKey = "&key=";
+const owlKey = "";
+
+module.exports = {  
+    token,
+    yandexKey,
+    imdbKey,
+    tenorKey,
+    owlKey,
+}
