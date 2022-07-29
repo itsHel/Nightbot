0dd5d1040ccb209c6c8f2a11bcdd853fbf1a87f8
itsHel <51739304+itsHel@users.noreply.github.com>
2022-07-29 19:43:46 +0200

    Update nightbot.js

--- nightbot.js
+++ nightbot.js
@@ -1,4 +1,5 @@
 const discord = require("discord.js");
+
 const reminder = require("./mymodules/reminder.js");
 const reddit = require("./mymodules/reddit.js");
 const mine = require("./mymodules/misc.js");
@@ -27,10 +28,10 @@ var lastList = "mymovies";
 var cahGames = [];
 var guilds; 
 
-// const allIntents = new discord.Intents(32767);
-// const client = new discord.Client({ allIntents });
+const allIntents = new discord.Intents(32767);
+const client = new discord.Client({ intents: allIntents, partials: ["CHANNEL"] });
 
-const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MESSAGES] });
+// const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MESSAGES] });
 
 // Init
 client.on("ready", async () => {
@@ -55,8 +56,10 @@ client.on("ready", async () => {
             } catch(err){console.log(err)}
         }
     }
-    
-    quotes.simpsons(channels[settings.defaultGuild].generalroom);
+
+    if(channels[settings.defaultGuild]){
+        quotes.simpsons(channels[settings.defaultGuild].generalroom);
+    }
 
     setIntervalsAll(guilds);
 });
@@ -65,6 +68,7 @@ client.login(keys.token);
 
 client.on("message", async message => {
     let guildId;
+
     if(message.guild){
         guildId = message.guild.id;
     } else {
@@ -695,7 +699,10 @@ function setIntervalsAll(guilds){
     // Every 20 - 28 hours send quote to main channel
     (function autoSend(){
         setTimeout(function(){
-            quotes.simpsons(channels[settings.defaultGuild].generalroom);
+            if(channels[settings.defaultGuild]){
+                quotes.simpsons(channels[settings.defaultGuild].generalroom);
+            }
+
             autoSend();
         }, (Math.floor(Math.random() * 9) + 20) * 3600000);       
     }());
